254355c40e55bf2cb19182e334bc2bde4f2e6cd9
Radhika Irkal <radhikamirkal@gmail.com>
2026-02-27 12:15:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -19,8 +19,8 @@ submit.onclick = () => {
     player1 = document.getElementById("player1").value || "Player1";
     player2 = document.getElementById("player2").value || "Player2";
 
-    setup.classList.add("hidden");
-    game.classList.remove("hidden");
+    setup.style.display = "none";
+    game.style.display = "block"; 
 
     message.textContent = `${player1}, you're up`;
 };
